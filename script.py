becaa4bd948592effe1b6271db6de0fa8e36e901
Ahmad Buhari <at.buhari@gmail.com>
2021-10-24 23:52:41 +0100

    test

--- script.py
+++ script.py
@@ -6,12 +6,13 @@
 
 #print(content)
 
+#print('Alex\\nAna!\\\nJoe\nJen')
+
 import math
 
 #x = math.pow(2.0, math.sqrt(9.0))
 #print(x)
 
-#print('Alex\\nAna!\\\nJoe\nJen')
 
 #print('{:.0f}'.format(1.5555))
 
@@ -20,42 +21,60 @@ import math
 #x = int(input())
 
 #print((user_num // x) , (((user_num // x) // x)) , (((user_num // x) // x ) // x))
+#help()
 
-#print(help(math))
+list_number = [1,2,3] 
 
-my_list = ['red','blue','green']
 
-print('\nColor Choices:')
 
-for x in my_list:
-    print(x)
+
+
+
+
+
+
+
+#################
+#List Exercis
+#################
+
+
+
+#my_list = ['red','blue','green']
+
+#print('\nColor Choices:')
+
+#for x in my_list:
+ #   print(x)
     
-my_list.append((input('\nPlease enter a new color Add new color \n')))
+#my_list.append((input('\nPlease enter a new color Add new color \n')))
 
-print('\nNew Color addition')
-for x in my_list:
-    print(x)
+#print('\nNew Color addition')
+#for x in my_list:
+ #   print(x)
 
 #removal of pop
-my_list.pop(2)
+#my_list.pop(2)
 
-print('\nRemoval Valdiation with pop index 2')
-for x in my_list:
-    print(x)
+#print('\nRemoval Valdiation with pop index 2')
+#for x in my_list:
+ #   print(x)
 
 
 #List removal with.remove method
-my_list.remove('red')
+#my_list.remove('red')
 
-print('\nRemoval Valdiation with remove \"red\"')
-for x in my_list:
-    print(x)
+#print('\nRemoval Valdiation with remove \"red\"')
+#for x in my_list:
+ #   print(x)
+
+#print('\nAdding another object')
 
 #List modificaton to list
-my_list[0] = 'red'
+#my_list[0] = 'red'
 
-print('\nAdding new object')
-for x in my_list:
-    print(x)
+#for x in my_list:
+ #   print(x)
 
-my_list.append
+#my_list.append(input('\nAdd another color'))
+
