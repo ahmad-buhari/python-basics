158dcd369d94d37b9c6a75cfe338fd1e6b0ba560
ahmad-buhari <at.buhari@gmail.com>
2022-04-20 00:19:39 +0200

    testing stdout

--- playground.py
+++ playground.py
@@ -1,9 +1,10 @@
 #playground tuotrial
 
-
+#importing the sys and time module
 import sys
 import time
 
+# function to write out str by wpm speed.
 def slow(x):
     for char in x:
         sys.stdout.write(char)
@@ -13,11 +14,14 @@ def slow(x):
 
 
 
-exit_key = ['q','quit']
+exit_key = ['q','quit', ' ']
 
-print(slow('Welcome to the python playground'))
+welcome_msg = 'Welcome to the python playground, with live console returns'
+
+
 while True:
     try:
+        slow(welcome_msg)
         user_input = (input('''\nSelect the following exercise to see live response
         \n1. Running the help() command
         \n2. Exploring the method of a class
@@ -28,11 +32,18 @@ while True:
 
         elif user_input == '1':
             print('help(str)')
+            slow('In this example we\'ll run the help command in the python console. Press Ctrl + C to exit the help menu')
+            print(help(str))
+            slow('Enter new selection')
+            break
+
+
         
         elif user_input =='3':
             slow('hello world, bye world3')
 
-    
+        slow('Enter Selection:\n')
+        user_input = input()
     except ValueError:
         slow('Exiting Window')
         
