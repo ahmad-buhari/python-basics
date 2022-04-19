b24c3acad3d9eb80c7ad73ab76eb0205493c0274
ahmad-buhari <at.buhari@gmail.com>
2022-04-20 01:12:29 +0200

    updating

--- playground.py
+++ playground.py
@@ -1,8 +1,11 @@
-#playground tuotrial
-
-#importing the sys and time module
+#loading os module to access system files
+import os 
 import sys
 import time
+
+path = './' #specifiying working file diretory
+repl_files = os.listdir(path) #calling os method to list directory (listdir)
+
 
 # function to write out str by wpm speed.
 def slow(x):
@@ -12,22 +15,24 @@ def slow(x):
         time.sleep(1./11)
 
 
-
-
 exit_key = ['q','quit', ' ']
 
 welcome_msg = 'Welcome to the python playground, with live console returns'
+slow(welcome_msg)
+
 
 
 while True:
     try:
-        slow(welcome_msg)
         user_input = (input('''\nSelect the following exercise to see live response
         \n1. Running the help() command
         \n2. Exploring the method of a class
-        \n3. Slow print\nType \'q\' or \'quite\' to exit the window\n''')) 
+        \n3. Display execrise files
+        \n4. Slow print example
+        \nType \'q\' or \'quit\' to exit the window\n''')) 
+      
         if user_input == 'q':
-            slow('Exiting Window')
+            slow('\nExiting Window, bye bye')
             break
 
         elif user_input == '1':
@@ -38,12 +43,14 @@ while True:
             break
 
 
+        elif user_input == '3':
+          [print(files) for files in repl_files if files[0] == 'l' ]
+          
         
-        elif user_input =='3':
-            slow('hello world, bye world3')
+        elif user_input =='4':
+            slow('hello world, bye world')
 
-        slow('Enter Selection:\n')
-        user_input = input()
-    except ValueError:
-        slow('Exiting Window')
+        slow('\n**restarting**\n')
         
+    except ValueError:
+        slow('\nExiting Window, bye bye')
